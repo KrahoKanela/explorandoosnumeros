bc42498c32f7c998ce85992f9087358938825d1e
KrahoKanela <66818070+KrahoKanela@users.noreply.github.com>
2020-08-17 02:10:39 -0300

    Primeiro Upload 0v1
    Versão 1 de testes

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1597640641,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"tiledbackground2.png",
+		"tiledbackground.png",
+		"spritebox-sheet0.png",
+		"spritebuttonstart_old-sheet0.png",
+		"spritebuttonstart-sheet0.png",
+		"cylindergeometrygenerator-sheet0.png",
+		"cylindergeometrygenerator-sheet1.png",
+		"mat100-sheet0.png",
+		"mat10-sheet0.png",
+		"mat1-sheet0.png",
+		"tones-and-i-dance-monkey-_lyrics_.ogg",
+		"marshmello, bastille happier lyrics-[convertirvideo.com].ogg",
+		"panic! at the disco  - into the unknown (lyrics).ogg",
+		"paradise - coldplay [lyrics].ogg",
+		"alessia cara - i choose (lyrics).ogg",
+		"happy - pharrell williams (lyrics) 🎵.ogg"
+	]
+}
